97ff91cf71f0caa24fe9727ec06f6e825d15955f
Vishal <vishalbhat.com@gmail.com>
2023-07-31 13:31:08 +0530

    Add project files.

---
+++ Migrations/20230731075755_initialModelsMIG.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FileImageUploadRetrieveDemoCoreMVC6.Migrations
+{
+    public partial class initialModelsMIG : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "FilesOnDatabase",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    FileType = table.Column<string>(nullable: true),
+                    Extension = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UploadedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: true),
+                    Data = table.Column<byte[]>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilesOnDatabase", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FilesOnFileSystem",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(nullable: true),
+                    FileType = table.Column<string>(nullable: true),
+                    Extension = table.Column<string>(nullable: true),
+                    Description = table.Column<string>(nullable: true),
+                    UploadedBy = table.Column<string>(nullable: true),
+                    CreatedOn = table.Column<DateTime>(nullable: true),
+                    FilePath = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FilesOnFileSystem", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FilesOnDatabase");
+
+            migrationBuilder.DropTable(
+                name: "FilesOnFileSystem");
+        }
+    }
+}
